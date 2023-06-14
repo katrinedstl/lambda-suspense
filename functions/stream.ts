d318c4b967bc2243b24824ca2c931f937d47b952
Katrine Dåstøl <katrine.dastol@sats.no>
2023-06-14 22:38:56 +0200

    use streams

--- functions/stream.ts
+++ functions/stream.ts
@@ -1,16 +1,15 @@
 import React from "react";
 import { renderToString } from "react-dom/server";
+import { streamifyResponse } from "lambda-stream"; // https://github.com/astuyve/lambda-stream
 
 import { App } from "../src/app";
 
-export async function handler() {
+export const handler = streamifyResponse(myHandler);
+
+async function myHandler(_event, responseStream) {
   const content = renderToString(React.createElement(App));
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "text/html",
-    },
-    body: content,
-  };
+  responseStream.setContentType("text/html");
+  responseStream.write(content);
+  responseStream.end();
 }
